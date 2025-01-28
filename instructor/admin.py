44f3802b2114fbfb71f7ec2908e33929d59b8c5f
sajay <sajaykannan10@gmail.com>
2025-01-28 12:18:14 +0530

    added course model

--- instructor/admin.py
+++ instructor/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from instructor.models import User
+from instructor.models import User,Category
 
 
 admin.site.register(User)
+
+admin.site.register(Category)
--- instructor/models.py
+++ instructor/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
 
+from embed_video.fields import EmbedVideoField
+
 class User(AbstractUser):
 
     ROLE_OPTIONS = (
@@ -35,6 +37,43 @@ def create_instructor_profile(sender,instance,created,**kwargs):
         InstructorProfile.objects.create(owner=instance)
 
 post_save.connect(create_instructor_profile,User)
+
+
+class Category(models.Model):
+
+    name=models.CharField(max_length=200,unique=True)
+
+    def __str__(self):
+        return self.name
+# python,datascience,machinelearning,deeplearning,Ai,statistics,
+# backend,frontend,ui/ux,fullstack
+
+
+class Course(models.Model):
+
+    title=models.CharField(max_length=200)
+
+    description=models.TextField()
+
+    price=models.DecimalField(decimal_places=2)
+
+    owner=models.ForeignKey(User,on_delete=models.SET_NULL,related_name="courses")
+
+    is_free=models.BooleanField(default=False)
+
+    picture=models.ImageField(upload_to="courseimages",null=True,blank=True,default="courseimages/default.png")
+
+    thumbnail=EmbedVideoField()
+
+    category_objects=models.ManyToManyField(Category)
+    
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
 
 
 
